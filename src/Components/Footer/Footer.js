ed4feef0d0da75855a047724dcd36aabd46745d2
Jordan Prescott <prescottjp@icloud.com>
2021-12-26 19:30:44 -0600

    Updating some margins and padding to help with responsiveness

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,7 +3,6 @@ import "./Footer.css";
 const Footer = () => {
     return(
         <div className="footer" id="footer-link">
-        
             <h1 className="contact">Contact Me</h1>
             <p className="contact-info"><span className="contact-color">Email</span> prescottjaustin@gmail.com</p>
             <p className="contact-info position"><span className="contact-color">Phone</span> 309-323-9601</p>
