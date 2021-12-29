ef06266863708673fbca230bf02cc110ac74bacb
Jordan Prescott <prescottjp@icloud.com>
2021-12-28 22:04:32 -0600

    I added links to all the skill cards.

--- src/Components/Skills/Skills.js
+++ src/Components/Skills/Skills.js
@@ -5,15 +5,39 @@ const Skills = () => {
     <div>
       <h1 id="about-link">Skills</h1>
       <div className="card-wrapper">
-        <div className="style">HTML</div>
-        <div className="style">CSS</div>
-        <div className="style">SASS</div>
-        <div className="style">Bootstrap</div>
-        <div className="style">JavaScript</div>
-        <div className="style">React</div>
-        <div className="style">Redux</div>
-        <div className="style">Git</div>
-        <div className="style">GitHub</div>
+        <a
+          href="https://developer.mozilla.org/en-US/docs/Web/HTML"
+          target="_blank"
+        >
+          <div className="style">HTML</div>
+        </a>
+        <a href="https://developer.mozilla.org/en-US/docs/Web/CSS">
+          <div className="style">CSS</div>
+        </a>
+        <a
+          href="https://www.geeksforgeeks.org/css-preprocessor-sass/"
+          target="_blank"
+        >
+          <div className="style">SASS</div>
+        </a>
+        <a href="https://getbootstrap.com/" target="_blank">
+          <div className="style">Bootstrap</div>
+        </a>
+        <a href="https://www.javascript.com/" target="_blank">
+          <div className="style">JavaScript</div>
+        </a>
+        <a href="https://reactjs.org/" target="_blank">
+          <div className="style">React</div>
+        </a>
+        <a href="https://redux.js.org/" target="_blank">
+          <div className="style">Redux</div>
+        </a>
+        <a href="https://git-scm.com/" target="_blank">
+          <div className="style">Git</div>
+        </a>
+        <a href="https://github.com/" target="_blank">
+          <div className="style">GitHub</div>
+        </a>
       </div>
     </div>
   );
